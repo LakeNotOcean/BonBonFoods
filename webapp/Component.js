51795b28eeb3b8f658bc8fb9cad355f7366ee6cb
Nevskiy Vladislav <VNevskii650@gmail.com>
2022-07-27 21:07:13 +0200

    initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,28 @@
+sap.ui.define(
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (UIComponent, JSONModel, ResourceModel) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.food.Component", {
+      metadata: {
+        interfaces: ["sap.ui.core.IAsyncContentCreation"],
+        manifest: "json",
+      },
+      init: function () {
+        // call the init function of the parent
+        UIComponent.prototype.init.apply(this, arguments);
+        // set data model
+        const oModel = new JSONModel(oData);
+        this.setModel(oModel);
+        // set i18n model on view
+        const i18nModel = new ResourceModel({
+          bundleName: "sap.ui.food.i18n.i18n",
+        });
+        this.setModel(i18nModel, "i18n");
+      },
+    });
+  }
+);
